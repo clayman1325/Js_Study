18fdd82cd47ef2464cec500bc3412abb49f7c121
davidfdomejiab <davidfdomejiab@gmail.com>
2022-11-14 10:32:39 -0500

    advance in the calculator project with basic interactive ploting

--- project/assets/scripts/app.js
+++ project/assets/scripts/app.js
@@ -1,1 +1,6 @@
-alert("this works!");
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = "Current" + " " + "Result"
+outputResult(currentResult, calculationDescription)
