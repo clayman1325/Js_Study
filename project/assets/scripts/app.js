42b3d58e1d2618e09c0368c6da1ae266c7d6e174
davidfdomejiab <davidfdomejiab@gmail.com>
2022-11-15 13:57:07 -0500

    add section for more strings constants and first assigment

--- project/assets/scripts/app.js
+++ project/assets/scripts/app.js
@@ -1,6 +1,8 @@
-let currentResult = 0;
+const initialValue = 0;
+let currentResult = initialValue;
 
 currentResult = currentResult + 10;
 
-let calculationDescription = "Current" + " " + "Result"
+let calculationDescription = `Current + Result ${initialValue}`
+let errorMessage = "An error " + "occurred"
 outputResult(currentResult, calculationDescription)
---
+++ section_2/basics-assignment-1-start/assignment.js
@@ -0,0 +1,8 @@
+let initialImput = 0;
+let result       = initialImput + 18
+
+result = result - 3;
+result = result * 3
+result = result / 5
+
+alert(` Your result is: ${result} and your initial Imput ${initialImput}`)
