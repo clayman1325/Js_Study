f52a16666b6c214a7530c12b1a88a9a158ef77ee
davidfdomejiab <davidfdomejiab@gmail.com>
2022-11-25 14:54:40 -0500

    practices quering and changing attributes via JS dom properties of HTML elemens

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,23 @@
+const allLis = document.querySelectorAll('li')
+
+for (let li of allLis){
+  li.style.color = "blue"
+  console.dir(li)
+}
+
+const listItemElements = document.getElementsByTagName("li")
+
+for (let li of listItemElements){
+  li.style.color = "green"
+  console.dir(li)
+}
+
+const h1 = document.getElementById("main-title");
+h1.textContent = " David estas haciendo las cosas bien y mejorando";
+h1.style.color = "white";
+h1.style.backgroundColor = "blue";
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + "Changed !"
+
+const body = document.body;
