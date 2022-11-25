181153b1703f2e448265d8d1992f8ed5b4ae4f46
davidfdomejiab <davidfdomejiab@gmail.com>
2022-11-25 16:02:59 -0500

    Dom manipulation assigment 1 DONEEEE!

---
+++ section_7/dom-assignment-start/assignment.js
@@ -0,0 +1,15 @@
+const one = document.querySelector("#task-1")
+one.style.color = "white"
+one.style.backgroundColor = "red"
+
+const oneV2 = document.getElementById("#task-1")
+one.style.color = "white"
+one.style.backgroundColor = "black"
+
+const title = document.querySelector("title")
+title.textContent = "Assigment - Solved!"
+
+document.title = "david trust and Patience"
+
+const h1 = document.querySelector("h1")
+h1.textContent = "Assigment - Solved!"
